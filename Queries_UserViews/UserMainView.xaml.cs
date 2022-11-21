e4f1de47b7ff54d8b89c3e54439180f051014dc4
Lili Chen <lilnic@ksu.edu>
2022-11-20 21:16:22 -0600

    checking search

--- Queries_UserViews/UserMainView.xaml.cs
+++ Queries_UserViews/UserMainView.xaml.cs
@@ -76,60 +76,9 @@ namespace UI.Queries_UserViews
 
         private void SpecsButton_Click(object sender, RoutedEventArgs e)
         {
+      //      OverrideBorder.Child = new SearchResults(this, UserID, (int)playlist.PlaylistID);
 
-            using (SqlConnection conn = new SqlConnection("Data Source=cis560-team3.database.windows.net;Initial Catalog=music;Persist Security Info=True;User ID=admin1;Password=Singtome!"))
-            {
-                conn.Open();
-                //musicDataSet.
-                // 1.  create a command object identifying the stored procedure
-                SqlCommand cmd = new SqlCommand("topCharts_proc", conn);
-
-                // 2. set the command object so it knows to execute a stored procedure
-                cmd.CommandType = CommandType.StoredProcedure;
-
-
-                // execute the command
-                using (SqlDataReader rdr = cmd.ExecuteReader())
-                {
-                    // iterate through results, printing each to console
-                    while (rdr.Read())
-                    {
-                        var song = new
-                        {
-                            SongName = rdr["SongName"],
-                            MusicianName = rdr["MusicianName"]
-                        };
-
-                        //TopChart.Items.Add(song.SongName + " - " + song.MusicianName);
-                    }
-
-
-                }
-
-                SqlCommand cmd2 = new SqlCommand("mostPlaylisted_proc", conn);
-
-                // 2. set the command object so it knows to execute a stored procedure
-                cmd2.CommandType = CommandType.StoredProcedure;
-
-
-                // execute the command
-                using (SqlDataReader rdr2 = cmd2.ExecuteReader())
-                {
-                    // iterate through results, printing each to console
-                    while (rdr2.Read())
-                    {
-                        var song = new
-                        {
-                            SongName = rdr2["SongName"],
-                            MusicianName = rdr2["MusicianName"]
-                        };
-
-                        //playlistChart.Items.Add(song.SongName + " - " + song.MusicianName);
-                    }
-
-                }
-            }
-
+           
         }
     }
 }
--- Queries_UserViews/UserSpecs.xaml.cs
+++ Queries_UserViews/UserSpecs.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,74 @@ namespace UI.Queries_UserViews
     /// </summary>
     public partial class UserSpecs : UserControl
     {
-        public UserSpecs()
+        UserMainView baseWindow;
+        int UserID;
+
+        public UserSpecs(UserMainView main, int userID)
         {
+
+
+
+
             InitializeComponent();
+
+            using (SqlConnection conn = new SqlConnection("Data Source=cis560-team3.database.windows.net;Initial Catalog=music;Persist Security Info=True;User ID=admin1;Password=Singtome!"))
+            {
+                conn.Open();
+                //musicDataSet.
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("genreBreakdown_proc", conn);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                // execute the command
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    // iterate through results, printing each to console
+                    while (rdr.Read())
+                    {
+                        var song = new
+                        {
+                            SongName = rdr["GenreName"],
+                            MusicianName = rdr["Proportion"]
+                        };
+
+                        //TopChart.Items.Add(song.SongName + " - " + song.MusicianName);
+                    }
+
+
+                }
+
+                SqlCommand cmd2 = new SqlCommand("mostPlaylisted_proc", conn);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd2.CommandType = CommandType.StoredProcedure;
+
+
+                // execute the command
+                using (SqlDataReader rdr2 = cmd2.ExecuteReader())
+                {
+                    // iterate through results, printing each to console
+                    while (rdr2.Read())
+                    {
+                        var rating = new
+                        {
+                            AvgRating = rdr2["AvgRating"],
+                        };
+
+                        //playlistChart.Items.Add(song.SongName + " - " + song.MusicianName);
+                    }
+
+                }
+            }
+
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-
+            baseWindow.OverrideBorder.Child = null;
         }
     }
 }
