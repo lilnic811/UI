bce9020c056ab236068f90814c657112f0383628
Lili Chen <lilnic@ksu.edu>
2022-11-20 20:47:29 -0600

    check

--- Queries_UserViews/UserMainView.xaml.cs
+++ Queries_UserViews/UserMainView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -70,6 +72,64 @@ namespace UI.Queries_UserViews
         private void CreatePlaylistButton_Click(object sender, RoutedEventArgs e)
         {
             OverrideBorder.Child = new Playlist_Creation(this, UserID);
+        }
+
+        private void SpecsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (SqlConnection conn = new SqlConnection("Data Source=cis560-team3.database.windows.net;Initial Catalog=music;Persist Security Info=True;User ID=admin1;Password=Singtome!"))
+            {
+                conn.Open();
+                //musicDataSet.
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("topCharts_proc", conn);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                // execute the command
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    // iterate through results, printing each to console
+                    while (rdr.Read())
+                    {
+                        var song = new
+                        {
+                            SongName = rdr["SongName"],
+                            MusicianName = rdr["MusicianName"]
+                        };
+
+                        TopChart.Items.Add(song.SongName + " - " + song.MusicianName);
+                    }
+
+
+                }
+
+                SqlCommand cmd2 = new SqlCommand("mostPlaylisted_proc", conn);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd2.CommandType = CommandType.StoredProcedure;
+
+
+                // execute the command
+                using (SqlDataReader rdr2 = cmd2.ExecuteReader())
+                {
+                    // iterate through results, printing each to console
+                    while (rdr2.Read())
+                    {
+                        var song = new
+                        {
+                            SongName = rdr2["SongName"],
+                            MusicianName = rdr2["MusicianName"]
+                        };
+
+                        playlistChart.Items.Add(song.SongName + " - " + song.MusicianName);
+                    }
+
+                }
+            }
+
         }
     }
 }
