554562f45c7200f490f4d995d4219745fbb4893c
Lili Chen <lilnic@ksu.edu>
2022-11-20 20:10:14 -0600

    agg procs done

---
+++ SQL Code/StoredProcedures/mostPlaylisted_proc.sql
@@ -0,0 +1,22 @@
+USE music;
+
+GO
+CREATE OR ALTER PROCEDURE dbo.mostPlaylisted_proc
+AS
+BEGIN
+SELECT TOP 5
+	S.SongName
+	,M.MusicianName
+	,COUNT(*) AS Appearances
+	FROM dbo.PlaylistSongs PS
+		INNER JOIN dbo.Songs S ON S.SongID = PS.SongID
+		INNER JOIN dbo.Albums A ON A.AlbumID =S.AlbumID
+		INNER JOIN dbo.Musicians M ON M.MusicianID = A.MusicianID
+GROUP BY S.SongName, M.MusicianName
+ORDER BY Appearances DESC
+
+END;
+
+
+
+--EXEC dbo.topCharts_proc
